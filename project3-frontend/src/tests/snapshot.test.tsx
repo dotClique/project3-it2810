4a62ec50ea4817d9cdf78eeee2611f729e92658b
Espen Boman Fosseide <espenbfo@stud.ntnu.no>
2021-10-16 14:24:48 +0200

    feat: add more snapshot tests

--- project3-frontend/src/tests/snapshot.test.tsx
+++ project3-frontend/src/tests/snapshot.test.tsx
@@ -2,13 +2,24 @@ import Enzyme, { shallow } from "enzyme";
 import toJson from "enzyme-to-json";
 import Adapter from "@wojtekmaj/enzyme-adapter-react-17";
 import MovieGroups from "../components/MovieGroup";
+import { FavoriteIcon } from "../components/FavoriteIcon";
+import MovieGroup from "../components/MovieGroup";
 
 Enzyme.configure({ adapter: new Adapter() });
 
 describe("Snapshot tests", () => {
-  // Snapshotting some components that many other components rely on and therefore should not be changed.
   test("Snapshotting MovieGroups", () => {
     const result = toJson(shallow(<MovieGroups />));
+    expect(result).toMatchSnapshot();
+  });
+
+  test("Snapshotting MovieGroup", () => {
+    const result = toJson(shallow(<MovieGroup />));
+    expect(result).toMatchSnapshot();
+  });
+
+  test("Snapshotting FavoriteIcon", () => {
+    const result = toJson(shallow(<FavoriteIcon />));
     expect(result).toMatchSnapshot();
   });
 });
