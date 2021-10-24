8a73032d6a81efc1133dc59ef48d5ea7ab6866d2
Espen Boman Fosseide <espenbfo@stud.ntnu.no>
2021-10-24 21:29:29 +0200

    feat: Split MovieEventPage into smaller components

--- project3-frontend/src/pages/MovieEventPage/index.tsx
+++ project3-frontend/src/pages/MovieEventPage/index.tsx
@@ -1,68 +1,19 @@
 import PageContainer from "../../components/PageContainer";
 import { Typography, Button, Card, Box, Divider } from "@mui/material";
 import { MovieGrid, MovieGroupsContainer } from "./styled";
-import {
-  GET_MOVIE_EVENT,
-  ADD_USER_TO_EVENT,
-  REMOVE_USER_FROM_EVENT,
-} from "../../helpers/graphql-queries";
-import { useQuery, useMutation } from "@apollo/client";
 import { useParams, useHistory } from "react-router-dom";
-import { useEffect, useState } from "react";
+import { useMovieEvent } from "./utils";
 
 export default function MovieEventPage() {
   const { id } = useParams() as { id: string };
   const history = useHistory();
-  const { data: dataEvents } = useQuery(GET_MOVIE_EVENT, {
-    variables: { movieEventId: String(id), alias: localStorage.getItem("alias") },
-    fetchPolicy: "network-only",
-  });
-
-  const [joinEvent, { data: joinData, loading: joinLoading, error: joinError }] = useMutation(
-    ADD_USER_TO_EVENT,
-    {
-      variables: { movieEventId: String(id), useralias: localStorage.getItem("alias") },
-    },
-  );
-
-  const [leaveEvent, { data: leaveData, loading: leaveLoading, error: leaveError }] = useMutation(
-    REMOVE_USER_FROM_EVENT,
-    {
-      variables: { movieEventId: String(id), useralias: localStorage.getItem("alias") },
-    },
-  );
-
-  const [isParticipant, setIsParticipant] = useState<boolean>(false);
-  useEffect(() => {
-    if (dataEvents)
-      setIsParticipant(
-        dataEvents && dataEvents.movieEvent ? dataEvents.movieEvent.userIsParticipant : [],
-      );
-  }, [dataEvents]);
-
-  useEffect(() => {
-    setIsParticipant(
-      leaveData && leaveData.removeUserFromEvent
-        ? leaveData.removeUserFromEvent.userIsParticipant
-        : isParticipant,
-    );
-  }, [leaveData]);
-
-  useEffect(() => {
-    setIsParticipant(
-      joinData && joinData.addUserToEvent
-        ? joinData.addUserToEvent.userIsParticipant
-        : isParticipant,
-    );
-  }, [joinData]);
-
-  console.log(isParticipant);
+  const { isParticipant, movieData, joinEvent, leaveEvent } = useMovieEvent(id);
   return (
     <PageContainer>
       <MovieGroupsContainer>
         <MovieGrid>
           <Typography color="primary" variant="h3" component="h3" sx={{ width: "100%" }}>
-            {dataEvents ? dataEvents.movieEvent.title : " "}
+            {movieData.title}
           </Typography>
 
           <Divider flexItem sx={{ width: "100%", margin: 1, borderColor: "primary.main" }} />
@@ -70,7 +21,7 @@ export default function MovieEventPage() {
             Description
           </Typography>
           <Typography variant="body1" id="beskrivelse" color="secondary.contrastText">
-            {dataEvents ? dataEvents.movieEvent.description : "kunne ikke laste inn"}
+            {movieData.description}
           </Typography>
           <Divider sx={{ width: "100%", margin: 1 }} />
           <Typography variant="h5" id="beskrivelse" color="primary">
@@ -78,7 +29,7 @@ export default function MovieEventPage() {
           </Typography>
           <Box sx={{ display: "flex", flexDirection: "row", gap: 4 }}>
             <Typography variant="body1" component="h4" color="secondary.contrastText">
-              {dataEvents ? dataEvents.movieEvent.location : "kunne ikke laste inn"}
+              {movieData.location}
             </Typography>
           </Box>
 
@@ -87,7 +38,7 @@ export default function MovieEventPage() {
             Date
           </Typography>
           <Typography variant="body1" component="h4" color="secondary.contrastText">
-            {dataEvents ? dataEvents.movieEvent.date : "--.--.----"}
+            {movieData.date}
           </Typography>
           <Divider sx={{ width: "100%", margin: 1 }} />
           <Typography variant="h5" id="beskrivelse" color="primary">
@@ -95,7 +46,7 @@ export default function MovieEventPage() {
           </Typography>
           <Box sx={{ display: "flex", flexDirection: "row", gap: 4 }}>
             <Typography variant="body1" component="h4" color="secondary.contrastText">
-              {dataEvents && isParticipant
+              {movieData && isParticipant
                 ? "You are a participant of this event"
                 : "You are not a participant of this event"}
             </Typography>
@@ -120,7 +71,7 @@ export default function MovieEventPage() {
             >
               Back
             </Button>
-            {dataEvents && isParticipant ? (
+            {movieData && isParticipant ? (
               <Button
                 variant={"contained"}
                 onClick={() => {
---
+++ project3-frontend/src/pages/MovieEventPage/utils.ts
@@ -0,0 +1,62 @@
+import { useMutation, useQuery } from "@apollo/client";
+import {
+  ADD_USER_TO_EVENT,
+  GET_MOVIE_EVENT,
+  REMOVE_USER_FROM_EVENT,
+} from "../../helpers/graphql-queries";
+import { useEffect, useState } from "react";
+
+export function useMovieEvent(id: string) {
+  const { data: dataEvents } = useQuery(GET_MOVIE_EVENT, {
+    variables: { movieEventId: String(id), alias: localStorage.getItem("alias") },
+    fetchPolicy: "network-only",
+  });
+
+  const [joinEvent, { data: joinData, loading: joinLoading, error: joinError }] = useMutation(
+    ADD_USER_TO_EVENT,
+    {
+      variables: { movieEventId: String(id), useralias: localStorage.getItem("alias") },
+    },
+  );
+
+  const [leaveEvent, { data: leaveData, loading: leaveLoading, error: leaveError }] = useMutation(
+    REMOVE_USER_FROM_EVENT,
+    {
+      variables: { movieEventId: String(id), useralias: localStorage.getItem("alias") },
+    },
+  );
+
+  const [isParticipant, setIsParticipant] = useState<boolean>(false);
+  useEffect(() => {
+    if (dataEvents)
+      setIsParticipant(
+        dataEvents && dataEvents.movieEvent ? dataEvents.movieEvent.userIsParticipant : [],
+      );
+  }, [dataEvents]);
+
+  useEffect(() => {
+    setIsParticipant(
+      leaveData && leaveData.removeUserFromEvent
+        ? leaveData.removeUserFromEvent.userIsParticipant
+        : isParticipant,
+    );
+  }, [leaveData]);
+
+  useEffect(() => {
+    setIsParticipant(
+      joinData && joinData.addUserToEvent
+        ? joinData.addUserToEvent.userIsParticipant
+        : isParticipant,
+    );
+  }, [joinData]);
+
+  const movieData = dataEvents
+    ? dataEvents.movieEvent
+    : {
+        title: "",
+        description: "",
+        location: "",
+        date: "--.--.----",
+      };
+  return { isParticipant, movieData, joinEvent, leaveEvent };
+}
