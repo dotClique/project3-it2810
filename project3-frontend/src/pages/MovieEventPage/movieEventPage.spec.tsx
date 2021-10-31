08c3423608e08d00312ebdff71639014034f1aa2
Espen Boman Fosseide <espenbfo@stud.ntnu.no>
2021-10-31 18:30:26 +0100

    feat: Add functionality tests in movieEventPage

--- project3-frontend/src/pages/MovieEventPage/movieEventPage.spec.tsx
+++ project3-frontend/src/pages/MovieEventPage/movieEventPage.spec.tsx
@@ -1,11 +1,15 @@
 import React from "react";
-import { render, screen, waitFor } from "@testing-library/react";
+import { fireEvent, render, screen, waitFor } from "@testing-library/react";
 import "@testing-library/jest-dom";
 import MovieEventPage from "./index";
 import { ThemeProvider } from "@mui/material";
 import theme from "../../helpers/themes";
-import { MockedProvider } from "@apollo/client/testing";
-import { GET_MOVIE_EVENT } from "../../helpers/graphql-queries";
+import { MockedProvider, MockedResponse } from "@apollo/client/testing";
+import {
+  ADD_USER_TO_EVENT,
+  GET_MOVIE_EVENT,
+  REMOVE_USER_FROM_EVENT,
+} from "../../helpers/graphql-queries";
 import { useParams } from "react-router";
 import { useAlias } from "../../helpers/alias";
 
@@ -13,7 +17,7 @@ jest.mock("react-router");
 jest.mock("../../helpers/alias");
 
 // Mock query result
-const mocks = [
+const mocks: MockedResponse[] = [
   {
     request: {
       query: GET_MOVIE_EVENT,
@@ -29,6 +33,38 @@ const mocks = [
           date: "2025-09-21T00:00:00.000",
           description: "testDescription",
           location: "testLocation",
+          userIsParticipant: true,
+        },
+      },
+    },
+  },
+  {
+    request: {
+      query: REMOVE_USER_FROM_EVENT,
+      variables: {
+        movieEventId: "testId",
+        useralias: "testPerson",
+      },
+    },
+    result: {
+      data: {
+        removeUserFromEvent: {
+          userIsParticipant: false,
+        },
+      },
+    },
+  },
+  {
+    request: {
+      query: ADD_USER_TO_EVENT,
+      variables: {
+        movieEventId: "testId",
+        useralias: "testPerson",
+      },
+    },
+    result: {
+      data: {
+        addUserToEvent: {
           userIsParticipant: true,
         },
       },
@@ -66,5 +102,49 @@ describe("Check That movieEventPage", () => {
     expect(screen.getByText("testLocation")).toBeInTheDocument();
     expect(screen.getByText("You are a participant of this event")).toBeInTheDocument();
     expect(screen.getByText("Leave Event")).toBeInTheDocument();
+  });
+
+  test("updates correctly when user leaves or joins event", async () => {
+    // Mock the url id
+    (useParams as jest.Mock).mockReturnValue({
+      id: "testId",
+    });
+
+    // Mock the locally stored user alias
+    (useAlias as jest.Mock).mockReturnValue({
+      alias: "testPerson",
+    });
+
+    render(
+      <MockedProvider mocks={mocks}>
+        <ThemeProvider theme={theme}>
+          <MovieEventPage />
+        </ThemeProvider>
+      </MockedProvider>,
+    );
+
+    // Wait for the query to run
+    await waitFor(() => new Promise((res) => setTimeout(res, 0)));
+
+    // Ensure that the user is joined
+    const leaveEventButton = screen.getByText("Leave Event");
+    expect(leaveEventButton).toBeInTheDocument();
+
+    // Leave the event
+    fireEvent.click(leaveEventButton);
+
+    // Wait for the query to run
+    await waitFor(() => new Promise((res) => setTimeout(res, 0)));
+
+    expect(screen.getByText("You are not a participant of this event")).toBeInTheDocument();
+    const joinEventButton = screen.getByText("Join Event");
+
+    // Join the event again
+    fireEvent.click(joinEventButton);
+
+    // Wait for the query to run
+    await waitFor(() => new Promise((res) => setTimeout(res, 0)));
+
+    expect(screen.getByText("You are a participant of this event")).toBeInTheDocument();
   });
 });
