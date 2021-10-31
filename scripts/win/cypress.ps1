47131e84c326d5fc89500f8305ffd7524279f44e
Ulrik Røsby <ulrik@rosby.no>
2021-10-31 19:52:28 +0100

    test: Setup test environment

---
+++ scripts/win/cypress.ps1
@@ -0,0 +1,5 @@
+# Script to create a testing ready instance of the containers. That is, the normal devcontainers but a non-persistent database.
+docker-compose -f .\docker-compose.test.yml down;
+docker-compose -f .\docker-compose.test.yml build;
+docker-compose -f .\docker-compose.test.yml up -d;
+# After this run: npm run cypress:open
